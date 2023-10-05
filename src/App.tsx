6a1cecc05966fcfa5cd6bf92c1edb7a2c32e5eb0
Emma Sargeant <emmasargeant@Emmas-MacBook-Pro.local>
2023-10-05 10:44:16 +0100

    set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './styles/tailwind.css';
-
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App w-full">
+      <h1 className='text-blue-400 text-center '> hello</h1>
+      </div>
   );
 }
 
